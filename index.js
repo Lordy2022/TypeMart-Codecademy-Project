da309fc229a851619faf2dc16966cb9bf4fe9d68
Kyle Lord <lordkyle@icloud.com>
2023-04-04 22:02:12 +0100

    adding data for shopper reciept

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ var product = products_1.default.filter(function (product) {
     return product.name === productName;
 })[0];
 console.log(product);
+if (product.preOrder) {
+    console.log("We will send you a message when your product ships.");
+}
