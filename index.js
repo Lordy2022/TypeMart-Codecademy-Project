39624874a6fa0b056ee714e28ec35914f3697a24
Kyle Lord <lordkyle@icloud.com>
2023-04-05 18:28:15 +0100

    if else statement free shipping for items under 25

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var products_1 = require("./products");
-var productName = 'shirt';
+var productName = 'fanny pack';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
+var shippingAddress = "42 Wallaby Way, Sidney, Australia.";
 var product = products_1.default.filter(function (product) {
     return product.name === productName;
 })[0];
-console.log(product);
 if (product.preOrder) {
     console.log("We will send you a message when your product ships.");
+}
+if (Number(product.price) > 25) {
+    console.log('This item will recieve free shipping');
+    shipping = 0;
+}
+else {
+    shipping = 5;
 }
--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
 import products from './products';
 
-const productName: string = 'shirt';
+const productName: string = 'fanny pack';
 let shipping: number;
 let taxPercent: number;
 let taxTotal: number;
 let total: number;
-let shippingAddress: string = "42 Wallaby Way, Sidney, Australia."
+let shippingAddress: string = "42 Wallaby Way, Sidney, Australia.";
 
 const product = products.filter(product => { 
   return product.name === productName;
 })[0];
 
-console.log(product);
-
-
 if (product.preOrder) {
   console.log("We will send you a message when your product ships.");
+}
+
+if (Number(product.price) > 25) {
+  console.log('This item will recieve free shipping');
+    shipping = 0;
+} else {
+  shipping = 5;
 }
 
